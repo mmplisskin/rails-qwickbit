69153ec9f0fe384d24009a3112c00b3c40ae9515
Samuel Chun <ephapox@gmail.com>
2015-07-07 17:23:02 -0700

    made styling changes and variable changes to javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,23 +23,23 @@
 
 var loadqrpub
 loadqrpub = function(){
-  var qrcode = new QRCode("pub_qrcode")
-  // var qrcode2 = new QRCode("priv_qrcode")
+  var qrcode_pub = new QRCode("pub_qrcode")
+  var qrcode_priv = new QRCode("priv_qrcode")
  function makeCode (){
     var elpub_ad = document.getElementById("pub_ad");
-    // var elpriv_ad = document.getElementById("priv_ad");
-    qrcode.makeCode(elpub_ad.value);
-    // qrcode2.makeCode(elpriv_ad.value);
+    var elpriv_ad = document.getElementById("priv_ad");
+    qrcode_pub.makeCode(elpub_ad.value);
+    qrcode_priv.makeCode(elpriv_ad.value);
   }
   makeCode();
   $("#pub_ad").
       on("load", function () {
           makeCode();
       })
-      // $("#priv_ad").
-      //     on("load", function () {
-      //         makeCode();
-      //     })
+      $("#priv_ad").
+          on("load", function () {
+              makeCode();
+          })
       on("keydown", function (e) {
           if (e.keyCode == 13) {
               makeCode();
