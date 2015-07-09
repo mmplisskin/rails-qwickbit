15e3ec70fd781947a9c1cf83f869e16d80dbcdfa
mmplisskin <mmplisskin@gmail.com>
2015-07-08 20:05:09 -0700

    fixes modal

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,17 @@ class StaticPagesController < ApplicationController
   def landing
     @business = Business.new
   end
+  def create
+    business = Business.find_by(email: params[:login][:email])
+    if business && business.authenticate(params[:login][:password])
+      # make a new key in our session object called :user_id
+      # which gets stored in a cookie in our browser
+      session[:business_id] = business.id.to_s
+      redirect_to business_path(current_business.id)
+    else
+      render :new
+    end
+  end
 
   def contact
   end
