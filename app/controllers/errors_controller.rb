cf978b329cef04b7d9726d010d259261e2f14d3a
mmplisskin <mmplisskin@gmail.com>
2015-07-17 08:13:55 -0700

    updates error to partial

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,11 @@
 class ErrorsController < ApplicationController
   def error404
-    render status: :not_found
+    # render status: :not_found
+    render 'layouts/error404'
   end
 
 
   def file_not_found
-    render status: :not_found
+    render 'layouts/error404'
   end
 end
