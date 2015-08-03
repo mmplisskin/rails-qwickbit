e23ec24fdcbac0883104773ac15450287fb85a21
mmplisskin <mmplisskin@gmail.com>
2015-08-03 16:04:54 -0700

    updates ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
+
+
+  force_ssl unless: -> { Rails.env.in? ['development', 'test'] }
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
